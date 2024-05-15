eda604a29a74e806009be95a4b50eaa0e94c3528
yuanke99 <2240247884@qq.com>
2024-05-15 20:47:18 +0800

    修正

--- src/main/java/group222/FirstMem2_1.java
+++ src/main/java/group222/FirstMem2_1.java
@@ -4,5 +4,7 @@ public class FirstMem2_1 {
     public void selfIntroduction(){
         System.out.println("大家好，我是2组rose");
         System.out.println("大家好，我是2组jane，rose不在了");
+        System.out.println("2组收到，规范化提交开始");
+        System.out.println("这是2组规范后的首次提交");
     }
 }
