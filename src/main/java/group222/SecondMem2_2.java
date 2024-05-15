ae6b07326a6cfbddc94e9471a6a0f4b9400a6b21
yuanke99 <2240247884@qq.com>
2024-05-15 20:18:36 +0800

    Rebased change2_1 on master，

--- src/main/java/group222/SecondMem2_2.java
+++ src/main/java/group222/SecondMem2_2.java
@@ -3,5 +3,6 @@ package group222;
 public class SecondMem2_2 {
     public void selfIntroduction(){
         System.out.println("大家好，我是2组jack");
+        System.out.println("Rebased change2_1 on master");
     }
 }
