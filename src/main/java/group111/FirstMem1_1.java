50fec941900a70dfffb6823f2720cb71d3ba514e
yuanke99 <2240247884@qq.com>
2024-05-15 20:44:11 +0800

    一组收到，规范化提交开始，这是一组规范后的首次提交

--- src/main/java/group111/FirstMem1_1.java
+++ src/main/java/group111/FirstMem1_1.java
@@ -6,5 +6,7 @@ public class FirstMem1_1 {
         System.out.println("一组张三，成为组长");
         System.out.println("基于分支change2_1创建新的分支，group1");
         System.out.println("未来这就是一组的工作空间了");
+        System.out.println("一组收到，规范化提交开始");
+        System.out.println("这是一组规范后的首次提交");
     }
 }
--- src/main/java/group111/SecondMem1_2.java
+++ src/main/java/group111/SecondMem1_2.java
@@ -5,5 +5,7 @@ public class SecondMem1_2 {
         System.out.println("大家好，我是一组王二");
         System.out.println("一组张三，成为组长");
         System.out.println("有请组长发言");
+        System.out.println("一组收到，规范化提交开始");
+        System.out.println("这是一组规范后的首次提交");
     }
 }
