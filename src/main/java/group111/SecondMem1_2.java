0e672dc31a50f0dd4073495b16eaec53e22260d2
yuanke99 <2240247884@qq.com>
2024-05-15 20:45:28 +0800

    2组收到，规范化提交开始，这是2组规范后的首次提交

--- src/main/java/group111/SecondMem1_2.java
+++ src/main/java/group111/SecondMem1_2.java
@@ -3,5 +3,7 @@ package group111;
 public class SecondMem1_2 {
     public void selfIntroduction(){
         System.out.println("大家好，我是一组王二");
+        System.out.println("2组收到，规范化提交开始");
+        System.out.println("这是2组规范后的首次提交");
     }
 }
--- src/main/java/group222/SecondMem2_2.java
+++ src/main/java/group222/SecondMem2_2.java
@@ -4,5 +4,7 @@ public class SecondMem2_2 {
     public void selfIntroduction(){
         System.out.println("大家好，我是2组jack");
         System.out.println("Rebased change2_1 on master");
+        System.out.println("2组收到，规范化提交开始");
+        System.out.println("这是2组规范后的首次提交");
     }
 }
