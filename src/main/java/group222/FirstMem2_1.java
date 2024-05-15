9896930dfa2a938185f96c201478a4cc19460196
yuanke99 <2240247884@qq.com>
2024-05-15 19:46:50 +0800

    change2_1，rose离开，jane来了。

--- src/main/java/group222/FirstMem2_1.java
+++ src/main/java/group222/FirstMem2_1.java
@@ -3,5 +3,6 @@ package group222;
 public class FirstMem2_1 {
     public void selfIntroduction(){
         System.out.println("大家好，我是2组rose");
+        System.out.println("大家好，我是2组jane，rose不在了");
     }
 }
