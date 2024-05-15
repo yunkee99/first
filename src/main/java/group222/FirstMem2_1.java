44cd260a909ccbd25ce0d1dca759549b730cf0a7
yuanke99 <2240247884@qq.com>
2024-05-15 20:51:00 +0800

    2组第3次提交：由2-1单独执行完成

--- src/main/java/group222/FirstMem2_1.java
+++ src/main/java/group222/FirstMem2_1.java
@@ -6,5 +6,6 @@ public class FirstMem2_1 {
         System.out.println("大家好，我是2组jane，rose不在了");
         System.out.println("2组收到，规范化提交开始");
         System.out.println("这是2组规范后的首次提交");
+        System.out.println("2组第3次提交：由2-1单独执行完成");
     }
 }
