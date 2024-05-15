24970e1b74b00009375c4e6afca876f9866a387e
yuanke99 <2240247884@qq.com>
2024-05-15 20:21:50 +0800

    merge group1 into master

--- src/main/java/group111/SecondMem1_2.java
+++ src/main/java/group111/SecondMem1_2.java
@@ -5,5 +5,6 @@ public class SecondMem1_2 {
         System.out.println("大家好，我是一组王二");
         System.out.println("一组张三，成为组长");
         System.out.println("有请组长发言");
+        System.out.println("merge group1 into master");
     }
 }
