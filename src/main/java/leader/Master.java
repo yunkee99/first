9f100f29b6bc634107cb92129833fccca81d2449
yuanke99 <2240247884@qq.com>
2024-05-15 20:59:09 +0800

    master同步2组2、3次更新失败，

--- src/main/java/leader/Master.java
+++ src/main/java/leader/Master.java
@@ -6,5 +6,9 @@ public class Master {
         System.out.println("Rebased change2_1 on master");
         System.out.println("merge group1 into master");
         System.out.println("master上线，提交规范化开始");
+        System.out.println("接收2组的两次提交");
+        System.out.println("merge change2 into master");
+        System.out.println("master的2组并没有更新");
+        System.out.println("刚刚2组更新弹窗选择选错了，那个意思是忽略，操，");
     }
 }
