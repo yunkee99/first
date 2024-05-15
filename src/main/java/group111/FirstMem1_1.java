257ca767bd53ad28dfd608efcc7b462c8d89b329
yuanke99 <2240247884@qq.com>
2024-05-15 19:54:49 +0800

    基于分支change2_1创建新的分支，group1， 未来这就是一组的工作空间了， 记录：一组张三，成为组长。

--- src/main/java/group111/FirstMem1_1.java
+++ src/main/java/group111/FirstMem1_1.java
@@ -3,5 +3,8 @@ package group111;
 public class FirstMem1_1 {
     public void selfIntroduction(){
         System.out.println("大家好，我是一组张三");
+        System.out.println("一组张三，成为组长");
+        System.out.println("基于分支change2_1创建新的分支，group1");
+        System.out.println("未来这就是一组的工作空间了");
     }
 }
