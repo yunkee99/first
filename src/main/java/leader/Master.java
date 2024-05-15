9b232c046e019ebe9d389b837745aa5608291f1f
yuanke99 <2240247884@qq.com>
2024-05-15 21:19:59 +0800

    获取二组第四次更新，更新成功，不仅第四次，2-3次更新也同步了

--- src/main/java/leader/Master.java
+++ src/main/java/leader/Master.java
@@ -10,5 +10,14 @@ public class Master {
         System.out.println("merge change2 into master");
         System.out.println("master的2组并没有更新");
         System.out.println("刚刚2组更新弹窗选择选错了，那个意思是忽略，操，");
+        System.out.println("master刚刚的操作再次丢失了，");
+        System.out.println("丢失操作如下" +
+                "Rebase change2_1 on master" +
+                "log merge and rebase" +
+                "merge change2 into master" +
+                "log merge2 failed");
+        System.out.println("获取二组第四次更新" +
+                "merge change2 into master");
+        System.out.println("更新成功，不仅第四次，2-3次更新也同步了");
     }
 }
